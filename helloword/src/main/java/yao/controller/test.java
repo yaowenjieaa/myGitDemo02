fab50420ff71dee8ad2b24e5a04ca05fcad1f55c
yaowenjieaa <53335729+yaowenjieaa@users.noreply.github.com>
2019-07-29 08:37:05 +0800

    Update test.java

--- helloword/src/main/java/yao/controller/test.java
+++ helloword/src/main/java/yao/controller/test.java
@@ -3,13 +3,9 @@ package yao.controller;
 public class test {
 
     public static void main(String[] args) {
+        System.out.println("aaa111");
         System.out.println("aaa");
         System.out.println("aaa");
-        System.out.println("aaa");
-        System.out.println("aaa");
-        System.out.println("aaa");
-        System.out.println("aaa");
-        System.out.println("aaa");
-        System.out.println("aaa");
+
     }
 }
