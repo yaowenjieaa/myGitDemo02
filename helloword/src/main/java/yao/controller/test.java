fde6284b9009f7e5a4217dee29d1665fddc1eb2c
root <root>
2019-07-29 08:33:25 +0800

    第二个项目

---
+++ helloword/src/main/java/yao/controller/test.java
@@ -0,0 +1,15 @@
+package yao.controller;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+    }
+}
